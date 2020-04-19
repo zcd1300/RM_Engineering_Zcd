85e15f9dd90a62e14f6a1412c8fc03a372fd3e8a
Zhang_cd <zcd1300@163.com>
2020-04-19 11:04:49 +0800

    master

--- App/Fuzzy_Controller.c
+++ App/Fuzzy_Controller.c
@@ -6,3 +6,47 @@
  * @Time 2020 1 5
 */
 
+#include "Fuzzy_Controller.h"
+
+
+
+/**
+ * @brief  Linear quantization operation function; 
+		   Range {-6~6}
+ * @param	Fuzzy *vPID,float pv,float *qValue
+ * @retval  void
+ * @Time 2020 4 19
+*/
+static void Linearization(Fuzzy *vPID,float fdb,float *qValue)
+{
+	float thisError;
+	float deltaError;
+	
+	thisError = vPID->SetPoint-fdb;			//本次误差
+	deltaError = thisError-vPID->LastERROR;	//上次误差
+	qValue[0] = 6.0*thisError/(vPID->max_out-vPID->min_out);  	//F(E)计算
+	qValue[1] = 3.0*deltaError/(vPID->max_out-vPID->min_out);	//F(Ec)计算 
+	
+}
+
+/**
+ * @brief  Membership calculation function
+ * @param	float *ms,float qv,int *Index
+ * @retval  void
+ * @Time 2020 4 19
+*/
+
+static void Membership_Calc(float *ms,float qv,int *Index)
+{
+	if((qv>=-NB)&&(qv<-NM))
+	{
+	
+		
+	}
+	
+	
+	
+	
+}
+
+
--- App/Fuzzy_Controller.h
+++ App/Fuzzy_Controller.h
@@ -5,3 +5,55 @@
  * @author ZCD
  * @Time 2020 1 5
 */
+
+#ifndef _Fuzzy_Controller_H
+#define _Fuzzy_Controller_H
+
+
+#include "stdint.h"
+
+#define NB -6
+#define NM -4
+#define NS -2
+#define ZO 0
+#define PS 2
+#define PM 4
+#define PB 6
+
+typedef struct
+{
+	float SetPoint;			//设定值
+	
+	float Kp;				
+	float Ki;
+	float Kd;
+	
+	float deadband;			//死区
+	
+	float LastERROR;		//上次偏差
+	float PreERROR;			//上上次偏差
+	
+	float output;			//输出
+	float result;			//物理量输出
+
+	float max_out;			//输出上限
+	float min_out;			//输出下限
+	
+	float max_Kp;			//Kp范围
+	float min_Kp;
+	float Q_Kp;				//Kp隶属度
+	
+	float max_Ki;
+	float min_Ki;
+	float Q_Ki;
+	
+	float max_Kd;
+	float min_Kd;
+	float Q_Kd;
+	
+}Fuzzy;
+
+
+
+#endif
+
