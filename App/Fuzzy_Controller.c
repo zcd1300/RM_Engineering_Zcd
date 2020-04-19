56bb6d98e40df608963c259b9bf3b0f6343aae97
Zhang_cd <zcd1300@163.com>
2020-04-19 16:33:57 +0800

    master

--- App/Fuzzy_Controller.c
+++ App/Fuzzy_Controller.c
@@ -31,22 +31,55 @@ static void Linearization(Fuzzy *vPID,float fdb,float *qValue)
 
 /**
  * @brief  Membership calculation function
- * @param	float *ms,float qv,int *Index
+ * @param	float *ms(membership),float qv(qValue),int *Index
  * @retval  void
  * @Time 2020 4 19
 */
 
 static void Membership_Calc(float *ms,float qv,int *Index)
 {
-	if((qv>=-NB)&&(qv<-NM))
+	if((qv>=NB)&&(qv<NM))
 	{
-	
-		
+		Index[0]=0;
+		Index[1]=1;
+		ms[0]= -0.5*qv-2.0;
+		ms[1]= 	0.5*qv+3.0;
 	}
-	
-	
-	
-	
+	else if((qv>=NM)&&(qv<NS))
+	{
+		Index[0]=1;
+		Index[1]=2;
+		ms[0]= -0.5*qv-1.0;
+		ms[1]= 	0.5*qv +2.0;
+	}
+	else if((qv>=NS)&&(qv<ZO))
+	{
+		Index[0]=2;
+		Index[1]=3;
+		ms[0]= -0.5*qv;
+		ms[1]=	0.5*qv+1.0;
+	}
+	else if((qv>=ZO)&&(qv<PS))
+	{
+		Index[0]=3;
+		Index[1]=4;
+		ms[0]= -0.5*qv+1.0;
+	    ms[1]=	0.5*qv;
+	}
+	else if((qv>=PS)&&(qv<PM))
+	{
+		Index[0]=4;
+		Index[1]=5;
+		ms[0]= -0.5*qv+2.0;
+		ms[1]=	0.5*qv-1.0;
+	}
+	else if((qv>=PM)&&(qv<=PB))
+	{
+		Index[0]=5;
+		Index[1]=6;
+		ms[0]= -0.5*qv+3.0;
+		ms[1]=	0.5*qv-2.0;
+	}
 }
 
 
